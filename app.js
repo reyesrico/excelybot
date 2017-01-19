f76fcefbed66718effe430f2652e412e92fbd8b1
Carlos Reyes <Carlos Reyes>
2017-01-18 22:50:48 -0800

    Adding first dialog.

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var connector = new builder.ChatConnector({
     appId: "977f1f37-5bf7-4dc9-b41a-92ce718885a7",
     appPassword: "Mi6VpXJtoqYiCo7E7hG2F4u"
 });
+
 var bot = new builder.UniversalBot(connector);
+
 server.post('/api/messages', connector.listen());
 
 //=========================================================
@@ -24,5 +26,25 @@ server.post('/api/messages', connector.listen());
 //=========================================================
 
 bot.dialog('/', function (session) {
-    session.send("Hello World");
+    session.beginDialog('questions');
 });
+
+bot.dialog('questions', [
+    function (session) {
+        builder.Prompts.text(session, "Hola! Como te llamas?");
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        builder.Prompts.number(session, "Hola " + results.response + "!, Cuantos años tienes de experiencia en Excel?"); 
+    },
+    function (session, results) {
+        session.userData.coding = results.response;
+        builder.Prompts.choice(session, "En que necesitas ayuda?", ["Modificar celdas", "Graficas", "Formulas", "Tablas Pivote", "Otro Tema"]);
+    },
+    function (session, results) {
+        session.userData.language = results.response.entity;
+        session.send("Listo " + session.userData.name + 
+                     "! Ya registramos que tienes " + session.userData.coding + 
+                     " años de experiencia y necesitas ayuda en " + session.userData.language + ".");
+    }
+]);
