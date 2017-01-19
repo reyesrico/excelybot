345b1bd4bf26bb4f2d62c2103471e06805763ca4
Carlos Reyes <Carlos Reyes>
2017-01-18 20:06:54 -0800

    Adding Microsoft AppID and AppPassword

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
   
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: "977f1f37-5bf7-4dc9-b41a-92ce718885a7",
+    appPassword: "Mi6VpXJtoqYiCo7E7hG2F4u"
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
